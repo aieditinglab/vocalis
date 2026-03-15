16ee6495e87bb814098fa9adf8caad7e6a369432
Ayden <aieditinglab@gmail.com>
2026-03-15 16:41:28 -0500

    fix: supabase client setup

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,4 @@
+'use client'
 import { createClient } from './supabase'
 import type { Session, UserSettings, AvatarConfig, GameScore } from './types'
 import { DEFAULT_SETTINGS, DEFAULT_AVATAR } from './types'
